4aedabbc647fa4ed13e02aeec31e1d0f8c3e00be
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2017-05-21 00:24:27 -0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
 gulp.task('log', function(){
     gutil.log('Workflows are awesome');
+});
+
+gulp.task('coffee', function(){
+    gulp.src('components/coffee/tagline.coffee')
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
