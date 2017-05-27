954c9d87a860be8ba7ea90937f970110b3cce4a4
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2017-05-26 21:57:26 -0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,58 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
-    concat = require('gulp-concat');
-    connect = require('gulp-connect');
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    cleanCss = require('gulp-clean-css'),
+    gulpIf = require('gulp-if');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];    
-var jsSources = [
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    htmlSources,
+    jsonSources,
+    outputDir, 
+    sassStyle,
+    condition;
+
+var env = process.env.NODE_ENV || 'dev';
+// if an environment variable has been set, use it, otherwise assume
+// that we are in a development environment
+
+if ( env === 'dev' ){
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+    condition = false;
+} else if ( env === 'prod' ) {
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+    condition = true;
+} else {
+    console.log( 'Error: invalid environment variable' );
+}
+
+coffeeSources = ['components/coffee/tagline.coffee'];    
+jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
 
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
 });
 
 gulp.task('coffee', function(){
@@ -33,7 +68,8 @@ gulp.task('js', function() {
   	gulp.src(jsSources)
     	.pipe(concat('script.js'))
     	.pipe(browserify())
-    	.pipe(gulp.dest('builds/development/js'))
+        .pipe(gulpIf(condition, uglify()))
+    	.pipe(gulp.dest(outputDir + 'js'))
         .pipe(connect.reload())
 });
 
@@ -41,11 +77,21 @@ gulp.task('compass', function() {
   	gulp.src(sassSources)
     	.pipe(compass({
     		sass: 'components/sass',
-    		image: 'builds/development/images'
+    		image: outputDir + 'images',
+            style: sassStyle
     	})
     	.on('error', gutil.log))
-    	.pipe(gulp.dest('builds/development/css'))
+        .pipe(gulpIf(condition, cleanCss()))
+    	.pipe(gulp.dest(outputDir + 'css'))
         .pipe(connect.reload())
+});
+
+gulp.task('connect', function() { // rem 'callback' == 'anonymous function'
+  connect.server({
+    root: outputDir,
+    port: 8000, // numbers don't need quotes
+    livereload: true // booleans don't need quotes
+  });
 });
 
 gulp.task('watch', function() {
@@ -58,20 +104,4 @@ gulp.task('watch', function() {
 
 gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
-gulp.task('connect', function() { // rem 'callback' == 'anonymous function'
-  connect.server({
-    root: 'builds/development',
-    port: 8000, // numbers don't need quotes
-    livereload: true // booleans don't need quotes
-  });
-});
 
-gulp.task('html', function() {
-    gulp.src(htmlSources)
-        .pipe(connect.reload())
-});
-
-gulp.task('json', function() {
-    gulp.src(jsonSources)
-        .pipe(connect.reload())
-});
